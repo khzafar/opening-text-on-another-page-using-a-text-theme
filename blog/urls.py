5e1a160e320bd9954b0952bddf876cbafc0fefbf
khzafar <zafarkhujakulov292@gmail.com>
2022-08-21 09:49:00 +0500

    change url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('blog/', BlogListView.as_view(), name='blog'),
+    path('', BlogListView.as_view(), name='blog'),
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
 ]
