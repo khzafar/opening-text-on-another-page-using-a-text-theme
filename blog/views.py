ebcdd786ef706c630f9060bbef2913ec80672528
khzafar <zafarkhujakulov292@gmail.com>
2022-08-21 09:13:52 +0500

    my blog web-site

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+import imp
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
